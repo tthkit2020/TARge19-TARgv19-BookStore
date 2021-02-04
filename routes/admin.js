1e5b4bfe2e49e774e821519fe8a7c245f69bf035
Julia Aasmaa <my_e_10@hotmail.com>
2021-02-04 15:49:28 +0200

    Add Edit

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,6 @@ router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product');
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
